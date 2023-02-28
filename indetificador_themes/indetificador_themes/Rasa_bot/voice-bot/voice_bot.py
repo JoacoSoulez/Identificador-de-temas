8dc7480cfa95e740961a56da86d46ab08455d621
Joaquin Soulez <joaco.soulez@gmail.com>
2023-02-27 23:16:13 -0300

    actualizacion

--- indetificador_themes/indetificador_themes/Rasa_bot/voice-bot/voice_bot.py
+++ indetificador_themes/indetificador_themes/Rasa_bot/voice-bot/voice_bot.py
@@ -1,48 +1,55 @@
+import speech_recognition as sr
+from IPython.display import Audio
+import os
+import ffmpeg
+import whisper
+from pydub import AudioSegment
+from scipy.io.wavfile import write
 import requests
-import speech_recognition as sr     # import the library
 import subprocess
 from gtts import gTTS
 
+
+
 bot_message = ""
-message=""
+message= ""
 
-r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": "Hola"})
-
-print("Bot says, ",end=' ')
-for i in r.json():
-    bot_message = 'Hola' #i['text']
+b = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": "hola"})
+print("Bot dice, ",end=' ')
+for i in b.json():
+    bot_message = i['text']
     print(f"{bot_message}")
-
-myobj = gTTS(text='hola')
+myobj = gTTS(text=bot_message, lang='es')
 myobj.save("welcome.mp3")
 print('saved')
 # Playing the converted file
-subprocess.call(['/Applications/VLC.app/Contents/MacOS/VLC', "welcome.mp3", '--play-and-exit'])
-
-while bot_message != "Bye" or bot_message!='thanks':
-
-    r = sr.Recognizer()  # initialize recognizer
-    with sr.Microphone() as source:  # mention source it will be either Microphone or audio files.
-        print("Speak Anything :")
-        audio = r.listen(source)  # listen to the source
+subprocess.call(['mpg321', "welcome.mp3", '--play-and-exit'])
+while bot_message != "Chau" or bot_message!='Gracias':
+    r = sr.Recognizer()
+    model = whisper.load_model("base")
+    #forced_decoder_ids = processor.get_decoder_prompt_ids(language="french", task="transcribe")
+    with sr.Microphone() as source:
+        print('Respondeme por favor...')
+        audio = r.listen(source)
+        with open('record.wav', 'wb') as f:
+            f.write(audio.get_wav_data())
         try:
-            message = r.recognize_google(audio)  # use recognizer to convert our audio into text part.
-            print("You said : {}".format(message))
-
+            result = model.transcribe('record.wav', language='es', verbose=False)
+            print('Dijiste: {}'.format(result["text"]))
         except:
-            print("Sorry could not recognize your voice")  # In case of voice not recognized  clearly
-    if len(message)==0:
+            print('no entendi, repita por favor')
+
+    if len(result)==0:
         continue
-    print("Sending message now...")
+    print("Enviando mensaje...")
 
-    r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": message})
+    b = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": str(result["text"])})
 
-    print("Bot says, ",end=' ')
-    for i in r.json():
+    print("Bot dice, ",end=' ')
+    for i in b.json():
         bot_message = i['text']
         print(f"{bot_message}")
-
-    myobj = gTTS(text=bot_message)
+    myobj = gTTS(text=bot_message, lang='es',tld='com.mx')
     myobj.save("welcome.mp3")
     print('saved')
     # Playing the converted file
